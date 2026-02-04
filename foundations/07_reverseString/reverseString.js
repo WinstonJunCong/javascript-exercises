183be3adc6e748d8a275790c0a267586b6d7ac18
WinstonJunCong <winstonjuncong@gmail.com>
2026-02-04 12:58:36 +0800

    complete 07 exercise

--- foundations/07_reverseString/reverseString.js
+++ foundations/07_reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
+const reverseString = function funcName(input) {
+    const start = input.length-1
+    let result = ''
 
+    for (i=start;i>=0;i--){
+        result += input[i]
+    }
+    return result
 };
 
 // Do not edit below this line
