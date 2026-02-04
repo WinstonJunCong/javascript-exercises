16c859d377050fdf8774525b2684d9f8fb5c560b
WinstonJunCong <winstonjuncong@gmail.com>
2026-02-04 12:48:56 +0800

    complete 06 exercise

--- foundations/06_repeatString/repeatString.js
+++ foundations/06_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
-
+const repeatString = function funcOne (a, b) {
+    let result = []
+    if (typeof b != 'number' || b < 0)  {
+        return 'ERROR'
+    }
+    
+    for (i=0;i<b;i++){
+        result.push(a)
+    }
+    return result.join("")
 };
 
 // Do not edit below this line
