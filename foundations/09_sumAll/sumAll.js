a0a93e6067b0440ce6003820d4323bbabe5036d5
WinstonJunCong <winstonjuncong@gmail.com>
2026-02-05 12:40:31 +0800

    complete 09 exercise

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,5 +1,12 @@
-const sumAll = function() {
-
+const sumAll = function(a,b) {
+    if (!Number.isInteger(a) || !Number.isInteger(b)) return "ERROR";
+    if (a <0 || b<0) return "ERROR";
+    if (a > b) [a, b] = [b, a]
+    let result = 0;
+    for (let i=a; i <=b; i++) {
+        result +=i;
+    }
+    return result;
 };
 
 // Do not edit below this line
